a735bc1e86e94a4f00ed53dcbd69a2ec388165c2
yutank34 <yuta34@gmail.com>
2021-01-01 22:59:49 +0900

    init

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("12:00")
+            Text(getDate())
+        }
+    }
+    
+    func getDate() -> String {
+        let f = DateFormatter()
+        f.dateFormat = "M月d日 EEE曜日"
+        f.locale = Locale(identifier: "ja_JP")
+        let now = Date()
+        return f.string(from: now)
+    }
+    
+    func getDef() -> String {
+        let f = DateFormatter()
+        f.timeStyle = .full
+        f.dateStyle = .full
+        f.locale = Locale(identifier: "ja_JP")
+        let now = Date()
+        return f.string(from: now)
     }
 }
 
