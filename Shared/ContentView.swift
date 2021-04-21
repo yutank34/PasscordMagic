bd0698729bd77f47c82645f2009d7e46a2a3e7cd
yutank34 <yuta34@gmail.com>
2021-04-21 23:07:50 +0900

    feat: modify top layout

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -11,44 +11,48 @@ struct ContentView: View {
     var now = Date()
     @State var isPresentedSubView = false
     var body: some View {
-        VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 2) {
-            Text(getTime())
-                .font(.system(size: 58))
-                .padding(.top, 30)
-            Text(getDate())
-            Spacer()
-            Button(action: {
-                self.isPresentedSubView.toggle()
-            }) {
-                Text("Press home to unlock")
+        ZStack {
+            Color.black.ignoresSafeArea()
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 2) {
+                Text(getTime())
+                    .font(.system(size: 72))
+                    .padding(.top, 60)
+                Text(getDate())
+                    .font(.system(size: 25))
+                Spacer()
+                Button(action: {
+                    self.isPresentedSubView.toggle()
+                }) {
+                    Text(NSLocalizedString("Unlock", comment: ""))
+                        .font(.system(size: 18))
+                        .padding(.bottom, 10)
+                }
+                .foregroundColor(.white)
+                .fullScreenCover(isPresented: $isPresentedSubView) {
+                    PasscordInput()
+                        .environmentObject(InputsManager())
+                }
+                Text("● ◇")
+                    .font(.system(size: 10))
+                    .padding(.bottom, 2)
             }
             .foregroundColor(.white)
-            .fullScreenCover(isPresented: $isPresentedSubView) {
-                PasscordInput()
-                    .environmentObject(InputsManager())
-            }
-            Text("● ◇")
-                .font(.system(size: 10))
-                .padding(.bottom, 2)
+            .frame(maxWidth: .infinity, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
+            .background(Color.black)
         }
-        .foregroundColor(.white)
-        .frame(maxWidth: .infinity, maxHeight: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, alignment: .center)
-        .background(Color.black)
-        
     }
     
     func getDate() -> String {
         let f = DateFormatter()
-        f.dateFormat = "M月d日 EEE曜日"
-        f.locale = Locale(identifier: "ja_JP")
+        f.dateFormat = NSLocalizedString("DateFormat", comment: "")
+        f.locale = Locale(identifier: NSLocalizedString("Locale", comment: ""))
         return f.string(from: now)
     }
     
     func getTime() -> String {
         let f = DateFormatter()
-        f.timeStyle = .short
-        f.dateStyle = .none
-        f.locale = Locale(identifier: "ja_JP")
+        f.dateFormat = NSLocalizedString("hh:mm", comment: "")
+        f.locale = Locale(identifier: NSLocalizedString("Locale", comment: ""))
         return f.string(from: now)
     }
 }
